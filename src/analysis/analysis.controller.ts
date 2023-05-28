eb80380bd6c63698653b3434c7f51c8eafa60d4d
shalikus <sarkisyan.deni@mail.ru>
2023-05-28 07:43:13 +0300

    refactor

--- src/analysis/analysis.controller.ts
+++ src/analysis/analysis.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, UseGuards } from '@nestjs/common'
 import { AnalysisService } from './analysis.service'
 import { CreateAnalysisDto } from './dto/create-analysis.dto'
-import { ApiTags } from '@nestjs/swagger'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 
 @ApiTags('Анализ')
 @Controller('analysis')
 export class AnalysisController {
   constructor(private readonly analysisService: AnalysisService) {}
 
+  @ApiOperation({ summary: 'Сделать анализ' })
+  @ApiResponse({ status: 200 })
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createAnalysisDto: CreateAnalysisDto) {
     return this.analysisService.create(createAnalysisDto)
   }
